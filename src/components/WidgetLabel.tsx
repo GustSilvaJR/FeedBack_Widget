4c06eb343829b34f9776267813a6a39b778a0d04
GustSilvaJR <gust.offic@gmail.com>
2022-05-16 14:14:10 -0300

    Adicionando botao close e algumas estilizacoes no proprio

--- src/components/WidgetLabel.tsx
+++ src/components/WidgetLabel.tsx
@@ -1,8 +1,11 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetLabel(){
     return(
         <div className="relative bg-zinc-700 flex flex-col rounded-2xl p-4 mb-4 items-center shadow-lg  w-[calc(100vw-2rem)] md:w-auto">
             <header>
                 <span className="text-xl leading-6">Deixe seu feedback</span>
+                <CloseButton/>
             </header>
 
             <footer>
